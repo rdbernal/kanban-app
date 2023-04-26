4a2b7310ebcd5c06685bb9b69127bce0ea8ff2f4
Rodrigo Bernal <rodrigobernal19@gmail.com>
2023-04-26 17:01:40 -0300

    style: add  member card component and responsive team page
    Add member card component and responsive team page

--- src/components/CustomInput/index.tsx
+++ src/components/CustomInput/index.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 export default function CustomInput({ label, type }: Props) {
   return (
-    <div className="flex flex-col gap-1">
+    <div className="flex flex-col gap-1 flex-1">
       <label>{label}</label>
       <input className="p-2 outline-none rounded" type={`${type}`} />
     </div>
--- src/pages/team/index.tsx
+++ src/pages/team/index.tsx
@@ -1,5 +1,32 @@
+// Components
+import CustomInput from "@/components/CustomInput";
+import Button from "@/components/Button";
+import Member from "@/components/Member";
+
 export default function Team() {
   return (
-    <h1>Team</h1>
+    <section className="bg-slate-200">
+      <section className="min-h-screen max-w-screen-2xl mx-auto p-4 flex flex-col gap-12">
+        <div className="flex flex-col gap-8 md:flex-row md:items-end md:justify-between md:gap-12">
+          <CustomInput label="Search for a member" type="text"/>
+
+          <Button tailwindClasses="w-full bg-violet-600 py-2 px-6 rounded-md text-slate-100 md:w-1/4 md:max-h-10">
+          Add member
+          </Button>
+        </div>
+
+        <div className="flex flex-col gap-8">
+          <h1 className="text-xl font-semibold text-violet-900 text-center">Team</h1>
+          <div className="flex flex-col gap-4 md:grid md:grid-cols-2 2xl:grid-cols-3">
+            <Member />
+            <Member />
+            <Member />
+            <Member />
+            <Member />
+            <Member />
+          </div>     
+        </div>
+      </section>
+    </section>
   )
 }
