9f14fa5333276bcdd8a272784056a6758699f4c9
Rodrigo Bernal <rodrigobernal19@gmail.com>
2023-04-27 15:01:44 -0300

    style: add board card component and responsive boards page
    Add board card component and responsive boards page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,36 @@
-// Fonts
-import { Inter } from "next/font/google"
-const inter = Inter({ subsets: ["latin"] })
+// Components
+import CustomInput from "@/components/CustomInput";
+import Button from "@/components/Button";
+import Board from "@/components/Board";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Boards</h1>
-    </main>
-  )
+    <section className="bg-slate-200">
+      <section className="min-h-screen max-w-screen-2xl mx-auto py-8 px-4 flex flex-col gap-12">
+        <header className="flex flex-col gap-8 md:flex-row md:items-end md:justify-between md:gap-12">
+          <CustomInput label="Search for boards" type="text" />
+
+          <Button tailwindClasses="w-full bg-violet-600 py-2 px-6 rounded-md text-slate-100 md:w-1/4 md:max-h-10">
+            Add Board
+          </Button>
+        </header>
+
+        <main className="flex flex-col gap-8">
+          <h1 className="text-xl font-semibold text-violet-900 text-center">Boards</h1>
+
+          <div className="flex flex-col gap-4 md:grid md:grid-cols-2 2xl:grid-cols-3">
+            <Board/>
+            <Board/>
+            <Board/>
+            <Board/>
+            <Board/>
+            <Board/>
+            <Board/>
+          </div>
+        </main>
+
+
+      </section>
+    </section>
+  );
 }
--- src/pages/team/index.tsx
+++ src/pages/team/index.tsx
@@ -6,16 +6,16 @@ import Member from "@/components/Member";
 export default function Team() {
   return (
     <section className="bg-slate-200">
-      <section className="min-h-screen max-w-screen-2xl mx-auto p-4 flex flex-col gap-12">
-        <div className="flex flex-col gap-8 md:flex-row md:items-end md:justify-between md:gap-12">
+      <section className="min-h-screen max-w-screen-2xl mx-auto py-8 px-4 flex flex-col gap-12">
+        <header className="flex flex-col gap-8 md:flex-row md:items-end md:justify-between md:gap-12">
           <CustomInput label="Search for a member" type="text"/>
 
           <Button tailwindClasses="w-full bg-violet-600 py-2 px-6 rounded-md text-slate-100 md:w-1/4 md:max-h-10">
           Add member
           </Button>
-        </div>
+        </header>
 
-        <div className="flex flex-col gap-8">
+        <main className="flex flex-col gap-8">
           <h1 className="text-xl font-semibold text-violet-900 text-center">Team</h1>
           <div className="flex flex-col gap-4 md:grid md:grid-cols-2 2xl:grid-cols-3">
             <Member />
@@ -25,7 +25,7 @@ export default function Team() {
             <Member />
             <Member />
           </div>     
-        </div>
+        </main>
       </section>
     </section>
   )
